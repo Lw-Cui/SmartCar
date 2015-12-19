3b301567ed1bdb67763d87513f74c6f2094592a0
Lw <645739460@QQ.com>
2015-12-19 19:53:21 +0800

    add dynamic velocity control.

--- App/control.c
+++ App/control.c
@@ -55,26 +55,62 @@ int16 get_offset(Point start, Point end) {
  *  @since      v1.0
  *	@reference
 		tpm_pwm_duty(TPM1,TPM_CH0,320);
-		330 - 425 - 550 int 舵机占空比
+		舵机占空比 int 330 - 425 - 550
 
+		电机占空比 int 1000
 		tpm_pwm_duty(TPM0,TPM_CH0,50);	
 		tpm_pwm_duty(TPM0,TPM_CH1, 0);
  */
-#define THRESHOLD 20
 void direction(Point new_dir[], int16 len) {
 	static Point start = {EMPTY, EMPTY}, end = {EMPTY, EMPTY};
 
-	if (len > THRESHOLD || empty(start) || empty(end)) {
+	/*最后应该写入 rom */
+	static int v_max = 150, v_min = 50;
+	static int v_max_cnt = 0, v_min_cnt = 0;
+#define VINIT 100
+	static int velocity = VINIT;
+	
+#define LEN 20
+	if (len > LEN || empty(start) || empty(end)) {
 		set(&start, new_dir[len - 1].x, (new_dir[len - 1].y + CAMERA_W / 2) / 2);
 
 		int end_x = new_dir[len / 4].x * 0.1 + new_dir[len / 2].x * 0.6 + new_dir[len * 3 / 4].x * 0.3;
 		int end_y = new_dir[len / 4].y * 0.1 + new_dir[len / 2].y * 0.6 + new_dir[len * 3 / 4].y * 0.3;
 		set(&end, end_x, end_y);
+
+		int16 offset = get_offset(start, end);
+		tpm_pwm_duty(TPM1,TPM_CH1, MID + offset);
+
+		if (abs(offset) < 50) {
+
+			if (velocity < v_max)
+				velocity += 10;
+			else
+				v_max_cnt++;
+
+		} else {
+
+			if (velocity > v_min)
+				velocity -= 10;
+			else 
+				v_min_cnt++;
+		}
+
+	} else {
+		int16 offset = get_offset(start, end);
+		tpm_pwm_duty(TPM1,TPM_CH1, MID + offset);
 	}
 
-	int16 offset = get_offset(start, end);
-	tpm_pwm_duty(TPM1,TPM_CH1, MID + offset);
 
-	tpm_pwm_duty(TPM0,TPM_CH0, 50);
+#define HIT 10
+	if (v_max_cnt > HIT) {
+		v_max += 100;
+		v_max_cnt = 0;
+	} else if (v_min_cnt > HIT) {
+		v_min -= 5;
+		v_min_cnt = 0;
+	}
+
+	tpm_pwm_duty(TPM0,TPM_CH0, velocity);
 	tpm_pwm_duty(TPM0,TPM_CH1,0);
 }
